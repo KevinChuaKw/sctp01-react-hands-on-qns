daac4b48a9672e57a9f6d4b4fb3aa8da05b5b58c
Kevin Chua <Kevin_chua3@hotmail.com>
2024-03-13 11:11:26 +0000

    trying to sort dice throw react app

---
+++ 04-dice-throw/04-dice-throw/src/Dice.js
@@ -0,0 +1,9 @@
+import {useState} from 'react'; 
+import './dice.css'; 
+
+export default function Roll() {
+    const [number, setNumber] = useState(1);
+    return <div className='dice'>
+    
+    </div>
+}; 
