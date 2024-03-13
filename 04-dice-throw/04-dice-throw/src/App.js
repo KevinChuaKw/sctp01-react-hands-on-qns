1d76b547654d447fd3ee7293a44931a6d918f3bd
Kevin Chua <Kevin_chua3@hotmail.com>
2024-03-13 13:16:53 +0000

    Fixing dice

--- 04-dice-throw/04-dice-throw/src/App.js
+++ 04-dice-throw/04-dice-throw/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-
-const [] = useState(""); 
-
-
+import Dice from './Dice';
 
 function App() {
   return (
--- 04-dice-throw/04-dice-throw/src/Dice.js
+++ 04-dice-throw/04-dice-throw/src/Dice.js
@@ -1,9 +1,15 @@
-import {useState} from 'react'; 
-import './dice.css'; 
+import { useState } from 'react';
+import './dice.css';
 
-export default function Roll() {
+export default function Dice() {
     const [number, setNumber] = useState(1);
     return <div className='dice'>
-    
-    </div>
+        {number}
+        <div>
+            <button onClick={() => {
+                setNumber(Math.floor(Math.random() * 6+ 1))
+            }}>Roll
+            </button>
+        </div>
+    </div >
 }; 
